cf95f423743291b2b612484b87f84ca5c236bda6
Akanshaa18 <akanborg@gmail.com>
2025-08-20 13:58:55 -0700

    Fixed bugs and cleaned code

--- newtonUnivariate.py
+++ newtonUnivariate.py
@@ -1,12 +1,13 @@
-import math
-
 def firstDerivative(x,f,h=1e-5):
+    """find the first derivative of given function"""
     return (f(x + h) - f(x - h)) / (2 * h)
 
 def secondDerivative(x,f,h=1e-5):
+    """find the second derivative of given function"""
     return (f(x + h) - 2 * f(x) + f(x - h)) / (h ** 2)
 
 def newtonUnivariate(x0,f,h, stoppingTolerance):
+    """find the newton univariate"""
     x = x0
     max_iter = 100
     for i in range(max_iter):
@@ -24,10 +25,13 @@ def newtonUnivariate(x0,f,h, stoppingTolerance):
 
         x = x_new
         return x
-
+        
+def f(x):
+    """function to test newton method"""
+    return x**2 + 2*x + 1
+    
 if __name__ == '__main__':
     x0= 5.0
-    f = lambda x: x**2 + 2*x + 1
     res = 0
     h = 1e-5
     stoppingTolerance = 1e-6
